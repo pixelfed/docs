71fa78e5c03edc1554867109a49866fa95979007
Daniel Supernault <danielsupernault@gmail.com>
2019-05-26 20:51:50 -0600

    update

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -4,15 +4,7 @@ module.exports = {
   base: '/docs/',
 
   head: [
-    [
-      'link',
-      {
-        href:
-          'https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,800,800i,900,900i',
-        rel: 'stylesheet',
-        type: 'text/css',
-      },
-    ],
+
   ],
 
   serviceWorker: false,
